804df89c53704a0b4cda64cd145a421fa12b0332
daxtonparke <daxtonparke@gmail.com>
2022-01-10 18:57:53 -0700

    complete

--- game.py
+++ game.py
@@ -1,3 +1,30 @@
-"""A number-guessing game."""
+import random
+print('welcome')
+increment = 0
 
-print('hi')
+def logic(rannum,increment):
+    guess = raw_input('guess a number between 1-100\n')
+    increment += 1
+    if type(guess) != int:
+        print('please just enter an int')
+    elif guess < 0 or guess> 100:
+        print('lol enter an int between 1-100')
+    elif guess < rannum:
+        print('too low')
+        logic(rannum,increment)
+    elif guess > rannum:
+        print('too high')
+        logic(rannum,increment)
+    else:
+        print('spot on')
+        print('it only took '+ str(increment) +' guesses')
+        
+def game():
+    global increment
+    rannum = random.randint(1,100)
+    print(rannum)
+    name = raw_input('what is your name? ')
+    print('hello '+name+'')
+    logic(rannum,increment)
+# print('rannum', rannum)
+game()
